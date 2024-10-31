48b70ff614a147238c6b36d0b60d68c216475f34
rehmankhan786 <arkhan.rock473@gmail.com>
2024-10-31 14:34:34 +0530

    upodated logOut functionality

--- middlewares/genToken.js
+++ middlewares/genToken.js
@@ -16,7 +16,7 @@ const genToken = async (user, res) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60,
-      sameSite: process.env.NODE_ENV == "development" ? "lax" : "none",
+      sameSite: process.env.NODE_ENV == "development" ? "Lax" : "None",
       secure: process.env.NODE_ENV == "development" ? false : true,
     })
     .json({ success: true, message: "login successfully", cookie: token,data });
