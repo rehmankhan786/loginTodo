94eeb5f173605b610ba18596ab5e249684621243
rehmankhan786 <arkhan.rock473@gmail.com>
2024-10-31 13:59:57 +0530

    upodated logOut functionality 2

--- middlewares/genToken.js
+++ middlewares/genToken.js
@@ -10,13 +10,14 @@ const genToken = async (user, res) => {
   console.log(email)
   const data = await userModel.findOne({email}).select("+password");
   const token = jwt.sign(user, process.env.secret);
+  console.log(process.env.NODE_ENV)
 
   return res
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60,
-      sameSite: process.env.NODE_ENV == "production" ? "lax" : "none",
-      secure: process.env.NODE_ENV == "production" ? false : true,
+      sameSite: process.env.NODE_ENV == "development" ? "lax" : "none",
+      secure: process.env.NODE_ENV == "development" ? false : true,
     })
     .json({ success: true, message: "login successfully", cookie: token,data });
   };
@@ -24,11 +25,11 @@ const genToken = async (user, res) => {
     console.log("Cookie Cleared")
   return res
     .cookie("token", "", {
-      httpOnly: true,
+      // httpOnly: true,
       // maxAge: 1,
-      expired:Date(Date.now()),
-      sameSite: process.env.NODE_ENV == "development" ? "lax" : "none",
-      secure: process.env.NODE_ENV == "development" ? false : true,
+      expired:new Date(Date.now()),
+      // sameSite: process.env.NODE_ENV == "development" ? "lax" : "none",
+      // secure: process.env.NODE_ENV == "development" ? false : true,
     })
     .json({ success: true, message: "logOut successfully", cookie: "" });
 
